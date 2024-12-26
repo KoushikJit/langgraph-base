efea0fd4c3943c15526d42e413a7b51016ce69d0
Koushik Sarkar <koushiksarkar@Koushiks-MacBook-Pro.local>
2024-12-26 13:03:20 +0530

    new update

--- agent.py
+++ agent.py
@@ -6,6 +6,11 @@ from langgraph.graph import add_messages, StateGraph, START
 class GraphState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
 
+def node1(state: GraphState) -> str:
+    return {"messages": BaseMessage(content="Hello", name="node1")}
+
 graph_builder = StateGraph(GraphState)
+graph_builder.add_node("node1", node1)
+graph_builder.add_edge(START, "node1")
 
 graph = graph_builder.compile()
